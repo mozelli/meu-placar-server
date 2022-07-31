c2c656190b639478bd413e882d4de451ac8f555f
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-07-31 19:19:09 -0300

    call authentication middleware

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,5 @@
 const routes = require("express").Router();
+const authentication = require("../middlewares/auth");
 
 // CONTROLLERS
 const Users = require("../controllers/UsersController");
@@ -10,5 +11,10 @@ routes.post("/sign-in", (request, response) => {
 routes.post("/add-new-user", (request, response) => {
   Users.AddNewUser(request, response);
 });
+
+// routes.use(authentication);
+// routes.get("/protected", (request, response) => {
+//   return response.json({ message: "Authorized.", user: request.userId });
+// });
 
 module.exports = { routes };
