4fe756c7a10f8a152963985b8290db4df00b13ae
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-07-31 15:32:07 -0300

    create mongoose config

---
+++ src/database/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL).then(() => {
+  console.log("Database connected!");
+});
+
+mongoose.Promise = global.Promise;
+
+export const db = mongoose;
