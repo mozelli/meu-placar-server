487edfcaa73e58f8e5ff38f6d637e81b3e4f407e
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-07-31 17:24:03 -0300

    create Users model

---
+++ src/models/Users.js
@@ -0,0 +1,45 @@
+const mongoose = require("../database/mongoose");
+
+const UsersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    require: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  emailValidationToken: {
+    type: String,
+    select: false,
+  },
+  state: {
+    type: String,
+    default: "awaiting_validation",
+  },
+  permition: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Users = mongoose.model("Users", UsersSchema);
+
+module.exports = Users;
