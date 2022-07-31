6369ee5d7f0c655a1d4559da04a9d9efbce285e0
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-07-31 15:28:48 -0300

    create Login and AddNewUser functions

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,36 @@
 module.exports = {
+  /**
+   * Login Function
+   * @Params request, response
+   * @Return User Object {id, name, lastname, birthday, email, token}
+   */
   Login(request, response) {
+    const { email, password } = request.body;
     return response.json({
-      message: "Login",
+      email,
+      password,
+    });
+  },
+
+  /**
+   * Login Function
+   * @Params request.body {name, lastname, email, password, birthday: {day, month, year}, permition}
+   * @Return boolean
+   */
+  AddNewUser(request, response) {
+    const { name, lastname, email, password, birthday, permition } =
+      request.body;
+    const { day, month, year } = birthday;
+
+    const birthdayTimestamp = new Date(`${year}-${month}-${day}`);
+
+    return response.json({
+      name,
+      lastname,
+      email,
+      password,
+      birthday: birthdayTimestamp,
+      permition,
     });
   },
 };
