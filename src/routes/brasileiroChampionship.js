c38fd8189565ab3f2a69998dab3550921d19e243
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-08-01 14:31:15 -0300

    create getTable route

--- src/routes/brasileiroChampionship.js
+++ src/routes/brasileiroChampionship.js
@@ -2,22 +2,23 @@ const routes = require("express").Router();
 const {
   scrapTable,
   scrapMatches,
+  getTable,
 } = require("../controllers/BrasileiroChampionshipController");
 const { requestLog } = require("../utils/logRegister");
 // import { getTable, scrapTable, getMatches, scrapMatches } from '../controllers/brasileiroChampionshipController';
 
 routes.get("/scrap-table/:championship", (request, response) => {
-  requestLog("GET", "scrap-table");
+  requestLog("GET", "brasileiro-championship/scrap-table");
   scrapTable(request, response);
 });
 
-// router.get("/table/:championship", (request:Request, response:Response) => {
-//   console.log(`[Request: /table/${request.params.championship}]`);
-//   getTable(request, response)
-// });
+routes.get("/table/:championship", (request, response) => {
+  requestLog("GET", "brasileiro-championship/table");
+  getTable(request, response);
+});
 
 routes.get("/scrap-matches/:championship", (request, response) => {
-  requestLog("GET", "matches");
+  requestLog("GET", "brasileiro-championship/scrap-matches");
   scrapMatches(request, response);
 });
 
