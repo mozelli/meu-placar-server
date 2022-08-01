5b75b801df0a0d856e17c8b58cd635fca93f2219
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-08-01 07:36:17 -0300

    register terminal logs

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,22 @@
 const routes = require("express").Router();
 const authentication = require("../middlewares/auth");
+const { requestLog } = require("../utils/logRegister");
 
 // CONTROLLERS
 const Users = require("../controllers/UsersController");
 
 routes.post("/sign-in", (request, response) => {
+  requestLog("POST", "sign-in");
   Users.SignIn(request, response);
 });
 
 routes.post("/add-new-user", (request, response) => {
+  requestLog("POST", "add-new-user");
   Users.AddNewUser(request, response);
 });
 
 routes.get("/email-validation/:token", (request, response) => {
+  requestLog("POST", "email-validation");
   Users.EmailValidation(request, response);
 });
 
