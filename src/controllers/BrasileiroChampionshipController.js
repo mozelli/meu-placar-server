7cb7e42f70d8a49452808c08f25855a754b6e714
Joao Mozelli Neto <joaomozelli@hotmail.com>
2022-08-01 13:38:33 -0300

    create scrapMatches in BrasileiroChampionshipController

--- src/controllers/BrasileiroChampionshipController.js
+++ src/controllers/BrasileiroChampionshipController.js
@@ -1,6 +1,8 @@
 const TableSerieA = require("../models/TableSerieA");
 const TableSerieB = require("../models/TableSerieB");
-const { leaderboard } = require("../modules/webscraping");
+const MatchesSerieA = require("../models/MatchesSerieA");
+const MatchesSerieB = require("../models/MatchesSerieB");
+const { leaderboard, matches } = require("../modules/webscraping");
 const { responseLog, successLog } = require("../utils/logRegister");
 
 module.exports = {
@@ -59,6 +61,62 @@ module.exports = {
           500,
           error.message,
           "BrasileiroChampionshipController.js, scrapTable(), leaderboard()"
+        );
+        return response.status(500).json(error.message);
+      });
+  },
+
+  async scrapMatches(request, response) {
+    const serie = request.params.championship;
+
+    matches(request)
+      .then((responseDataMatches) => {
+        let Matches;
+
+        if (serie === "A") {
+          Matches = MatchesSerieA;
+        } else {
+          Matches = MatchesSerieB;
+        }
+
+        Matches.deleteMany()
+          .then(() => {
+            successLog(`Collection MatchesSerie${serie} successfully cleaned.`);
+
+            Matches.create(responseDataMatches)
+              .then(() => {
+                responseLog(
+                  "success",
+                  201,
+                  `New collection MatchesSerie${serie} successfully created.`
+                );
+              })
+              .catch((error) => {
+                responseLog(
+                  "error",
+                  500,
+                  error.message,
+                  "BrasileiroChampionshipController.js, scrapMatches(), matches(), Matches.deleteMany(), Matches.create()"
+                );
+                return response.status(500).json(error.message);
+              });
+          })
+          .catch((error) => {
+            responseLog(
+              "error",
+              500,
+              error.message,
+              "BrasileiroChampionshipController.js, scrapMatches(), matches(), Matches.deleteMany()"
+            );
+            return response.status(500).json(error.message);
+          });
+      })
+      .catch((error) => {
+        responseLog(
+          "error",
+          500,
+          error.message,
+          "BrasileiroChampionshipController.js, scrapMatches(), matches()"
         );
         return response.status(500).json(error.message);
       });
